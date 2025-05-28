b707542d796785dcc6674571e90a7b925481442b
Igor Zinovyev <zinigor@gmail.com>
2025-05-28 21:04:27 +0300

    Initialized the package.

---
+++ example/dart_comfy_frontend_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_comfy_frontend/dart_comfy_frontend.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dart_comfy_frontend_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_comfy_frontend/dart_comfy_frontend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
